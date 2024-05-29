19cb4a9d8983cbf1606dfe8fdcfd8378475e0767
Михаил Смирнов <migel1976@gmail.com>
2024-05-29 14:23:50 +0300

    test history api

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const button = document.getElementById('myButton');
-button.addEventListener('click', function () {
-    // console.log('click');
-    alert('click')
-  });
+const box=document.querySelector('.box');
 
-const button1 = document.getElementById('secondButton');
-button1.addEventListener('click', function () {
-    // console.log('click');
-    alert('Second button clicked')
-  });
+const pageActivity=document.getElementById('pgActivity');
+pageActivity.addEventListener('click', ()=>{
+  window.history.pushState({page:'activity'}, 'activity', 'activity.html');
+  box.innerHTML=history.state;
+})
+
+const pageMap=document.getElementById('pgMap');
+pageMap.addEventListener('click', ()=>{
+  window.history.pushState({page:'map'}, 'map', 'map.html');
+  box.innerHTML=history.state;
+})
