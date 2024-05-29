f939cac23661c5f1a2e24c87f46587376b9e8876
Михаил Смирнов <mike@MacBook-Pro-Mihail.local>
2024-05-29 10:40:01 +0300

    another button

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ button.addEventListener('click', function () {
     // console.log('click');
     alert('click')
   });
+
+const button1 = document.getElementById('secondButton');
+button.addEventListener('click', function () {
+    // console.log('click');
+    alert('Second button clicked')
+  });
